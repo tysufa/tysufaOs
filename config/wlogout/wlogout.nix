093489a2e4ce4a3ef01c959d8f58f37a1686d246
tysufa <philemon.penot@gmail.com>
2024-10-07 14:03:44 +0200

    better wlogout configuration

--- config/wlogout/wlogout.nix
+++ config/wlogout/wlogout.nix
@@ -33,5 +33,63 @@
         keybind  = "u";
       }
     ];
+  style = ''
+    window {
+    font-family: CaskaydiaCove Nerd Font, monospace;
+    font-size: 12pt;
+    color: #cdd6f4; 
+    background-color: rgba(0, 0, 0, .9);
+}
+
+button {
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 25%;
+    border: none;
+    color: #fbf1c7;
+    text-shadow: none;
+    border-radius: 20px 20px 20px 20px;
+    background-color: rgba(1, 121, 111, 0);
+    margin: 5px;
+    transition: box-shadow 0.2s ease-in-out, background-color 0.2s ease-in-out;
+}
+
+#lock {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/lock.png"));
+}
+/*
+#lock:focus {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/lock-hover.png"));
+}
+*/
+#logout {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/logout.png"));
+}
+#logout:focus {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/logout-hover.png"));
+}
+
+#suspend {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/sleep.png"));
+} 
+#suspend:focus {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/sleep-hover.png"));
+}
+
+#shutdown {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/power.png"));
+}
+#shutdown:focus {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/power-hover.png"));
+}
+
+#reboot {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/restart.png"));
+}
+#reboot:focus {
+    background-image: image(url("/home/tysufa/tysufaOs/config/wlogout/restart-hover.png"));
+}
+    '';
   };
+  
 }
