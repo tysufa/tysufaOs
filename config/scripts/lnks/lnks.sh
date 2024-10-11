94d808151f4ff93f63876946be234d903d19eff3
tysufa <philemon.penot@gmail.com>
2024-10-12 01:08:05 +0200

    small changes in lnks to make it feet more to my needs

--- config/scripts/lnks/lnks.sh
+++ config/scripts/lnks/lnks.sh
@@ -10,19 +10,27 @@ if ! type fzf &> /dev/null; then
 fi
 
 keep_open=false
-dir="$(dirname "$0")"
+baseDir="$(dirname "$0")"
+curDir=$(pwd)
 
 usage () {
     echo "Usage: $(basename "$0") [OPTIONS...]"
     echo "  -k        --keep-open     Keep lnks open after selecting a bookmark"
     echo "  -d <dir>  --dir <dir>     Specify a directory where bookmarks files are stored"
     echo "  -e        --edit          Edit the bookmark file"
+    echo "  -c        --create        Create the bookmark file if it does not exist"
     exit 0
 }
 
 edit () {
-  vim "$dir"/*.txt
+  vim "$curDir"/.bookmarks.txt
   exit 0
+}
+
+create(){
+  if [[ ! -e "$curDir"/.bookmarks.txt ]]; then
+    touch "$curDir"/.bookmarks.txt
+  fi
 }
 
 while [[ "$#" -gt 0 ]]; do
@@ -31,31 +39,37 @@ while [[ "$#" -gt 0 ]]; do
         -d|--dir) dir="$2"; shift ;;
         -h|--help) usage ;;
         -e|--edit) edit ;;
+        -c|--create) create ;;
         *) echo "Unknown parameter passed: $1" >&2; exit 1 ;;
     esac
     shift
 done
 
+if [[ -e "$curDir"/.bookmarks.txt ]]; then
+  dir=$curDir
+else
+  dir=$baseDir
+fi
 if type explorer.exe &> /dev/null; then
-    open_command="explorer.exe"
+  open_command="explorer.exe"
 elif type open &> /dev/null; then
-    open_command="open"
+  open_command="open"
 elif type xdg-open &> /dev/null; then
-    open_command="xdg-open"
+  open_command="xdg-open"
 fi
 
 enter_command="enter:execute-silent(${open_command} {-1})"
 
 if [ "$keep_open" = true ]; then
-    enter_command="${enter_command}+clear-query"
+  enter_command="${enter_command}+clear-query"
 else
-    enter_command="${enter_command}+abort"
+  enter_command="${enter_command}+abort"
 fi
 
-cat "$dir"/*.txt | fzf \
-    --border=rounded \
-    --prompt="Search Bookmarks > " \
-    --with-nth='1..-2' \
-    --bind="${enter_command}" \
-    --preview='echo {-1}' \
-    --preview-window='up,1'
+cat "$dir"/.bookmarks.txt | fzf \
+  --border=rounded \
+  --prompt="Search Bookmarks > " \
+  --with-nth='1..-2' \
+  --bind="${enter_command}" \
+  --preview='echo {-1}' \
+  --preview-window='up,1'
