a4ee63733041e5a7381fd6c7767cf93e93ac5ad6
tysufa <philemon.penot@gmail.com>
2025-12-08 21:53:49 +0100

    removed annoying notif in nvim

--- moon/config/nvim/lua/config/lazy.lua
+++ moon/config/nvim/lua/config/lazy.lua
@@ -23,11 +23,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
 	spec = {
-
-		{ import = "lazyvim.plugins" },
-
-		-- import your plugins
+		-- import/override with your plugins
 		{ import = "plugins" },
+
 		-- colorschemes
 		{ "rebelot/kanagawa.nvim" },
 		{ "catppuccin/nvim", name = "catppuccin" },
@@ -59,7 +57,7 @@ require("lazy").setup({
 
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
+	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
