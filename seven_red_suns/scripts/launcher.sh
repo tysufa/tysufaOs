dd18f14f0086acb4f199621d306bd419a59a6ddc
tysufa <philemon.penot@gmail.com>
2026-08-24 19:28:32 +0200

    trying the launcher vicinae instead of wofi

---
+++ seven_red_suns/scripts/launcher.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ----- WOFI LAUNCHER -----
+
+# CONFIG="$HOME/.config/wofi/config/config"
+# STYLE="$HOME/.config/wofi/src/mocha/style.css"
+#
+# if [[ ! $(pidof wofi) ]]; then
+#     wofi --show drun --conf "${CONFIG}" --style "${STYLE}"
+# else
+#     pkill wofi
+# fi
+
+
+# -------- VICINAE LAUNCHER ------------
+vicinae toggle
